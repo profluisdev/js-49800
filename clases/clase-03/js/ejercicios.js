89597f8e0a66668df921378658eb0f4a918fada0
Luis Mera <profeluismeradev@gmail.com>
2023-11-17 21:55:16 -0300

    Ejercicios para la clase 3

---
+++ clases/clase-03/js/ejercicios.js
@@ -0,0 +1,28 @@
+// Todos los ejercicios tiene que utilizar los temas vistos en clase hasta el momento, variables, condicionales y ciclos
+// Pueden realizar menús como el visto en clase, queda a criterio del programador
+
+/* 
+Ejercicio 1: Realizar un programa que solicite al usuario que ingrese un número, luego imprimir por consola solo los números
+pares dese el 1 al número ingresado. Para saber cual es el número para se tiene que utilizar algo llamado
+resto % "Búscalo en Google" :D  
+*/
+
+/* 
+Ejercicios 2: Realizar un programa que sea un juego de adivinar el número, para ello se debe:
+- Determina en una variable un número fijo el cual se debe adivinar entre el 1 y el 10.
+- Solicitar al usuario que ingrese un número en un rango del 1 al 10, si no lo hace solicitarle nuevamente
+que ingrese el número indicando que tiene que ser del 1 al 10. 
+- En caso de que ingrese un número del 1 al 10 y no adivine, solicitar nuevamente un número, si quiere salir
+del programa y no seguir intentando que ingrese las letras ESC, esto pone al fin al ciclo, configurar que ESC no sea keySensitive
+- En caso de adivinar el número mostrar un mensaje indicando que adivino el número y terminar el programa
+*/
+
+/* 
+Ejercicio 3: Realizar un programa de trivia de preguntas, en esta ocasión solo va a tener uan pregunta, 
+más adelante lo vamos a modificar para que pueda tener varias preguntas. 
+- Se debe hacer una pregunta al usuario.
+- El usuario tiene la opción de elegir entre 5 respuestas posibles.
+- En caso de no adivinar volver a solicitar al usuario una respuesta.
+- En caso de adivinar indicarle al usuario que esa es la respuesta correcta. 
+- (Opcional) Realizar un contador que sume la cantidad de intentos del usuario para adivinar la respuesta
+*/
